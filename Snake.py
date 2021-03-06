8545a928f2989a3eebb88428eaa4d096402645ee
MOhamed ELfeky <mohamedelfeky202020@gmail.com>
2021-03-06 09:45:46 +0200

    add movement of snake

--- Snake.py
+++ Snake.py
@@ -16,10 +16,13 @@ red = (255,0,0)
 #create window
 window = pygame.display.set_mode((WEIDTH,HEIGHT))
 pygame.display.set_caption("Snake")
-window.fill(bg)
+def draw_screen():
+    window.fill(bg)
 
 #game variables
 cell_size =10
+direction = 1 # 1 for up, 2 for right , 3 for down and 4 for left
+update_snake = 0
 
 #creat snake
 snake_pos = [[int(WEIDTH/2),int(HEIGHT/2)]]
@@ -32,10 +35,37 @@ snake_pos.append([int(WEIDTH/2),int(HEIGHT/2)+ cell_size*3])
 run = True
 
 while run:
+    draw_screen()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.KEYDOWN and direction !=3:
+            if event.key == pygame.K_UP:
+                direction = 1
+            if event.key == pygame.K_RIGHT and direction !=4:
+                direction = 2
+            if event.key == pygame.K_DOWN and direction !=1:
+                direction = 3
+            if event.key == pygame.K_LEFT and direction !=2:
+                direction = 4
+    if update_snake>99:
+        update_snake =0
+        
+        snake_pos = snake_pos[-1:] + snake_pos[:-1]
+        if direction == 1:
+            snake_pos[0][0] = snake_pos[1][0]
+            snake_pos[0][1] = snake_pos[1][1] - cell_size
+        if direction == 3:
+            snake_pos[0][0] = snake_pos[1][0]
+            snake_pos[0][1] = snake_pos[1][1] + cell_size
+        if direction == 2:
+            snake_pos[0][0] = snake_pos[1][0] + cell_size
+            snake_pos[0][1] = snake_pos[1][1] 
+        if direction == 4:
+            snake_pos[0][0] = snake_pos[1][0] - cell_size
+            snake_pos[0][1] = snake_pos[1][1] 
     
+            
     #draw snake
     head = 1
     for x in snake_pos:
@@ -49,6 +79,7 @@ while run:
         
     #update the display
     pygame.display.update()
+    update_snake+=1
 
 
 pygame.quit()
