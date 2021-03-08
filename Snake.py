b87e19b69c85bba91f414a2c6ec63796cd17d7e0
MOhamed ELfeky <mohamedelfeky202020@gmail.com>
2021-03-08 04:28:31 +0200

    draw food

--- Snake.py
+++ Snake.py
@@ -12,17 +12,31 @@ bg = (255,200,150)
 body_inner = (50,175,25)
 body_outer = (100,100,200)
 red = (255,0,0)
+food_color = (200,50,140)
+score_color = (255,0,0)
+#define fon
+font = pygame.font.SysFont(None,40)
 
 #create window
 window = pygame.display.set_mode((WEIDTH,HEIGHT))
 pygame.display.set_caption("Snake")
+
 def draw_screen():
     window.fill(bg)
+
+def draw_score():
+    score_txt = "Score: " + str(score)
+    score_img = font.render(score_txt,True,score_color)
+    window.blit(score_img,(0,0))
 
 #game variables
 cell_size =10
 direction = 1 # 1 for up, 2 for right , 3 for down and 4 for left
 update_snake = 0
+food = [0,0]
+new_food = True
+new_piece = [0,0]
+score = 0
 
 #creat snake
 snake_pos = [[int(WEIDTH/2),int(HEIGHT/2)]]
@@ -36,11 +50,12 @@ run = True
 
 while run:
     draw_screen()
+    draw_score()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        elif event.type == pygame.KEYDOWN and direction !=3:
-            if event.key == pygame.K_UP:
+        elif event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_UP and direction !=3:
                 direction = 1
             if event.key == pygame.K_RIGHT and direction !=4:
                 direction = 2
@@ -48,7 +63,39 @@ while run:
                 direction = 3
             if event.key == pygame.K_LEFT and direction !=2:
                 direction = 4
-    if update_snake>99:
+
+    if new_food == True :
+        new_food = False
+        #food cordinates
+        food[0] = cell_size * random.randint(0,(WEIDTH / cell_size)-1)
+        food[1] = cell_size * random.randint(0,(HEIGHT / cell_size)-1)
+        
+        
+    #draw food
+    pygame.draw.rect(window,food_color,(food[0],food[1],cell_size,cell_size))
+
+    #check if food is eaten
+    if snake_pos[0] == food :
+        new_food = True
+        #create new piece at last point at snake's tale
+        new_piece = list(snake_pos[-1])
+
+        if direction ==1:
+            new_piece[1] += cell_size
+        if direction ==3:
+            new_piece[1] -= cell_size
+        if direction ==2:
+            new_piece[0] -= cell_size
+        if direction ==4:
+            new_piece[0] += cell_size
+
+        #attach new piece
+        snake_pos.append(new_piece)
+
+        #update score
+        score +=1
+                    
+    if update_snake>60:
         update_snake =0
         
         snake_pos = snake_pos[-1:] + snake_pos[:-1]
